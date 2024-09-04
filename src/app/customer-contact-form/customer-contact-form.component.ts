390f129bda7f7499b4c3e3e5f7cdba6ff014aee2
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-09-04 08:27:01 -0600

    customerror

--- src/app/customer-contact-form/customer-contact-form.component.ts
+++ src/app/customer-contact-form/customer-contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors, FormControl, FormGroupDirective } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-customer-contact-form',
@@ -9,22 +10,40 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 export class CustomerContactFormComponent {
   form: FormGroup;
   fb: FormBuilder = new FormBuilder();
+  customErrorStateMatcher: ErrorStateMatcher = new CustomErrorStateMatcher();
+
 
   constructor(fb: FormBuilder) {
-      this.form = fb.group({
-      firstName: [ 'John' ],
-      lastName: [ 'Doe' ],
-      phoneNumbers: fb.array([fb.group({
-        alias: ['Home'],
-        number: ['555-555-5555']
-      })]),
+          this.form = fb.group({
+      firstName: [ 'John', Validators.required ],
+      lastName: [ 'Doe', Validators.required ],
+      email: [ 'john.doe@example.com', Validators.compose([ Validators.required, Validators.email ]) ],
+      phoneNumbers: fb.array([ fb.group({
+        alias: [ 'Home' ],
+        number: [ '555-555-5555' ]
+      }) ]),
       address: fb.group({
-        street: ['123 Main St.'],
-        city: ['Salt Lake City'],
-        state: ['UT'],
-        zip: ['84001']
+        street: [ '123 Main St.' ],
+        city: [ 'Salt Lake City' ],
+        state: [ 'UT' ],
+        zip: [ '84001' ]
       })
-    });
+    }, { validator: this.forbiddenFullNameValidator }); 
+  }
+  
+    forbiddenFullNameValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const firstName = control.get('firstName')?.value;
+    const lastName = control.get('lastName')?.value;
+    const fullName = `${ firstName } ${ lastName }`;
+    const forbidden = new RegExp(/^[Mm]ickey [Mm]ouse$/).test(fullName);
+    return forbidden ? { 'forbiddenFullName': { value: fullName } } : null;
+  }
+
+    forbiddenNameValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const forbidden = new RegExp(/mickey/i).test(control.value);
+      return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+    };
   }
 
     get phoneNumbers(): FormArray {
@@ -34,6 +53,7 @@ export class CustomerContactFormComponent {
     reset(): void {
       this.form.controls['firstName'].setValue('');
       this.form.controls['lastName'].setValue('');
+      this.form.controls['email'].setValue('');
   }
     fillDefaultAddress(): void {
     this.form.patchValue({
@@ -51,5 +71,11 @@ export class CustomerContactFormComponent {
       number: ['']
     }));
   }
-  
+}
+class CustomErrorStateMatcher implements ErrorStateMatcher {
+
+  isErrorState(control: FormControl, form: FormGroupDirective): boolean {
+    return form.getError('forbiddenFullName') || control.invalid;
+  }
+
 }
